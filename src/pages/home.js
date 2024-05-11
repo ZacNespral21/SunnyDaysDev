3e025fffbb9868263bd0fe4ff4ca444d4957dbdd
Zachary Nespral <zacharynespral@gmail.com>
2024-05-11 02:31:41 -0400

    force deploy

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavigationBar, FooterDesktop } from '@core';
 // import { BrowserView, MobileView } from 'react-device-detect';
-import HomeComp from 'components/home.component';
+import HomeComponent from 'components/home.component';
 
 function Home() {
   return <>
     <NavigationBar />
-      <HomeComp />
+      <HomeComponent />
     <FooterDesktop />
   </>
 }
