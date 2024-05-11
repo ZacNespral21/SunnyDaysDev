fcb02af6728f98cc40d95e822d1dd7979def4e6d
Zachary Nespral <zacharynespral@gmail.com>
2024-05-11 02:42:38 -0400

    log check

--- src/@core/scripts/supabase-client.js
+++ src/@core/scripts/supabase-client.js
@@ -6,6 +6,11 @@ const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
 // const supabaseServiceRole = process.env.REACT_APP_SUPABASE_SERVICE_ROLE
 
+if(!supabaseUrl){
+  console.log('!')
+  console.log(process.env.REACT_APP_SUPABASE_ANON_KEY)
+}
+
 const supaBase = createClient(supabaseUrl, supabaseAnonKey);
 
 // @ts-ignore
